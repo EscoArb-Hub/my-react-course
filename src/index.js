142503cfa08aa4f43c8f8d8d386366f06378b19e
PavloHorhaniuk <escoarb2025@gmail.com>
2026-09-15 20:05:33 +0200

    next 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; //! 1 - React-елементи
 
+import ReactDOM from 'react-dom/client'; //! 2 - Render React-елемента
+
+//! 1 - React-елементи
+// const element = React.createElement("div");
+// * document.createElement("div"); //* JS
+
+// const element = React.createElement("div", { a: 5, b: 10 }, "Привіт  світ!");
+
+// const element = React.createElement(
+//   "div",
+//   { a: 5, b: 10 },
+//   "Привіт ",
+//   " ",
+//   "світ!"
+// );
+
+const element = React.createElement("h1", {
+  a: 5,
+  b: 10,
+  children: ["Привіт ", " ", "світ!"]
+});
+
+console.log("element:", element);
+
+//! 2 - Рендер одного React-елемента
+//? ReactDOM.createRoot(document.getElementById('root')).render(element);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(element);
+//* root.append(element) //* JS
