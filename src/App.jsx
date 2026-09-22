b5e1724f441330e511a0f061a257d10c081e9e3f
PavloHorhaniuk <escoarb2025@gmail.com>
2026-09-22 19:56:34 +0200

    next 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import PaintingsList from './components/PaintingsList';
+
+
 import paintings from "./json/paintings.json";
 console.log("paintings:", paintings)
 
@@ -7,36 +10,36 @@ export default function App() {
     const data1 = paintings[1];
     const data2 = paintings[2];
 
-    function Painting(props) {
-        // function Painting({ url, title, author, profileUrl, price }) {
-        console.log("props:", props);
-        const { url, title, author, profileUrl, price } = props;
-        return <div>
-            <img src={url} alt={title} width="480" />
-            <h2>{title}</h2>
-            <p>Автор: <a href={profileUrl}>{author}</a></p>
-            <p>Цена: {price} кредитов</p>
-            <p>Доступность: заканчивается или есть в наличии</p>
-            <button type="button">Додати до кошику</button>
-        </div>
-    };
+    // function Painting(props) {
+    //     // function Painting({ url, title, author, profileUrl, price }) {
+    //     console.log("props:", props);
+    //     const { url, title, author, profileUrl, price } = props;
+    //     return <div>
+    //         <img src={url} alt={title} width="480" />
+    //         <h2>{title}</h2>
+    //         <p>Автор: <a href={profileUrl}>{author}</a></p>
+    //         <p>Цена: {price} кредитов</p>
+    //         <p>Доступность: заканчивается или есть в наличии</p>
+    //         <button type="button">Додати до кошику</button>
+    //     </div>
+    // };
     return (
         <>
-            <Painting
+            <PaintingsList
                 url={data0.url}
                 title={data0.title}
                 author={data0.author.tag}
                 profileUrl={data0.author.url}
                 price={data0.price}
             />
-            <Painting
+            <PaintingsList
                 url={data1.url}
                 title={data1.title}
                 author={data1.author.tag}
                 profileUrl={data1.author.url}
                 price={data1.price}
             />
-            <Painting
+            <PaintingsList
                 url={data2.url}
                 title={data2.title}
                 author={data2.author.tag}
